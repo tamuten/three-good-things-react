a1a0c08a43867b1d53546c6eb15faf7f3eb1c019
tamuten <tamuten310@gmail.com>
2022-05-16 22:32:10 +0900

    入力してstateに反映することは可能

---
+++ src/Good.jsx
@@ -0,0 +1,53 @@
+import { useState, useEffect } from "react";
+
+export const Good = (props) => {
+  const [good, setGood] = useState("");
+  const [count, setCount] = useState(good.length);
+  const [tmpGood, setTmpGood] = useState(good);
+  const [tmpCount, setTmpCount] = useState(count);
+  const [editMode, setEditMode] = useState(false);
+
+  const handleClick = () => setEditMode(!editMode);
+  const handleTextChange = (e) => {
+    setTmpGood(() => e.target.value);
+  };
+
+  useEffect(() => {
+    setTmpCount(tmpGood.length);
+  }, [tmpGood]);
+
+  useEffect(() => {
+    setCount(good.length);
+  }, [good]);
+
+  const handleCancelBtnClick = () => {
+    setTmpGood(() => good);
+    handleClick();
+  };
+
+  const handleSaveBtnClick = () => {
+    setGood(() => tmpGood);
+    handleClick();
+  };
+
+  return (
+    <>
+      {editMode ? (
+        <div>
+          <textarea
+            value={tmpGood}
+            onChange={handleTextChange}
+           
+          ></textarea>
+          <p>{tmpCount}</p>
+          <button onClick={handleCancelBtnClick}>キャンセル</button>
+          <button onClick={handleSaveBtnClick}>保存</button>
+        </div>
+      ) : (
+        <div className="edit">
+          <a onClick={handleClick}>{good ? good : "タップして編集する"}</a>
+        </div>
+      )}
+    </>
+  );
+};
--- src/index.js
+++ src/index.js
@@ -1,17 +1,89 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { useEffect, useState } from "react";
+import ReactDOM from "react-dom";
+import { Good } from "./Good";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const diaryList = [
+  {
+    date: new Date(2022, 4, 8),
+    good: [
+      "9万円の椅子が座り心地が良かった。欲しい。",
+      "西野七瀬はやっぱり可愛い",
+      "風呂を裸足で掃除すると冷たいので、風呂用の靴を買うか。",
+    ],
+  },
+  {
+    date: new Date(2022, 4, 9),
+    good: [
+      "ikzwさんに腰痛のアドバイスもらった。",
+      "パソコンスタンド買うかなぁ",
+      "表示がうまくいきますように",
+    ],
+  },
+  {
+    date: new Date(2022, 4, 10),
+    good: [
+      "今日は5月10日です",
+      "パソコンスタンドを買ったら、目線が上に行って良いかもしれない",
+      "表示はきっとうまくいくだろう",
+    ],
+  },
+];
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const equalDate = (expected, actual) => {
+  return (
+    expected.getDate() == actual.getDate() &&
+    expected.getMonth() == actual.getMonth() &&
+    expected.getFullYear() == actual.getFullYear()
+  );
+};
+
+const Diary = () => {
+  
+
+  // const [dt, setDt] = useState(() => new Date());
+  // const initializeDiary = () => {
+  //   for (let d of diaryList) {
+  //     if (equalDate(dt, d.date)) {
+  //        return d.good;
+  //     }
+  //   }
+  // }
+  // const [diary, setDiary] = useState(initializeDiary);
+
+  // const handlePrevBtn = () => {
+  //   setDt((prev) => prev.getDate() - 1);
+  // };
+
+  // const handleNextBtn = () => {
+  //   setDt((prev) => prev.getDate() + 1);
+  // };
+
+
+
+  // useEffect(() => {
+  //   for (let d of diaryList) {
+  //     if (equalDate(dt, d.date)) {
+  //       setDiary(() => d.good);
+  //     }
+  //   }
+  // }, [dt]);
+
+  return (
+    <>
+      {/* <ul>
+        <li>
+          <button >prev</button>
+        </li>
+        <li>
+          <button o>next</button>
+        </li>
+      </ul>
+      <p></p> */}
+      <Good  />
+      <Good  />
+      <Good  />
+    </>
+  );
+};
+
+ReactDOM.render(<Diary />, document.getElementById("root"));
